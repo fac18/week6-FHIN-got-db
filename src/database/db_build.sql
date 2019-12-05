4ac996d94fdcd3ffa4a3c733e48f766883fc9125
ReginaldJbeili <reginaldjbeili@gmail.com>
2019-12-05 11:25:15 +0000

    add sql file

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -6,7 +6,8 @@ DROP TABLE IF EXISTS characters, houses, weapons, powers, honour CASCADE;
 CREATE TABLE characters (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL
+    last_name VARCHAR(100) NOT NULL,
+    honour INTEGER NOT NULL
 );
 
 CREATE TABLE houses (
@@ -21,17 +22,52 @@ CREATE TABLE weapons (
     character_id INTEGER REFERENCES characters(id) 
 );
 
-CREATE TABLE powers (
-    id SERIAL PRIMARY KEY,
-    level INTEGER NOT NULL,
-    character_id INTEGER REFERENCES characters(id) 
-)
+INSERT INTO characters(first_name,last_name,honour) VALUES
+('Jon', 'Snow',100),
+('Daenerys', 'Targaryen', 140),
+('Ice', 'King', 230),
+('Ramsay', 'Bolton', 80),
+('Petyr', 'Baelish', 40),
+('Arya', 'Stark', 120),
+('Cersei', 'Lannister', 40),
+('Robb', 'Stark', 90),
+('Jamie', 'Lannister', 110),
+('Tyrion', 'Lannister', 2),
+('Stannis', 'Baratheon', 90),
+('Balon', 'Greyjoy', 70),
+('Oberyn', 'Martell', 115),
+('Margaery', 'Tyrell', 3),
+('Brienn', 'of Tarth', 113),
+('Yara', 'Greyjoy', 86),
+('Ellaria', 'Sand', 94),
+('Red', 'Woman', 133),
+('Missandei', 'of Naath', 23),
+('Olena', 'Martell', 1);
 
-CREATE TABLE honour (
-    id SERIAL PRIMARY KEY,
-    points INTEGER NOT NULL,
-    character_id INTEGER INTEGER REFERENCES characters(id) 
-)
+INSERT INTO houses(name) VALUES
+('House Stark'),
+('House Targaryen'),
+('House Bolton'),
+('House Baelish'),
+('House Lannister'),
+('House Greyjoy'),
+('House Martell'),
+('House Tyrell'),
+('House Tarth'),
+('House Baratheon'),
+('No Allegiances');
 
+INSERT INTO weapons(name) VALUES
+('broad sword'),
+('dagger'),
+('wildfire'),
+('poison'),
+('whip'),
+('axe'),
+('mind'),
+('short sword'),
+('bow and arrow'),
+('dragon'),
+('direwolf');
 
-COMMIT;
+COMMIT; 
