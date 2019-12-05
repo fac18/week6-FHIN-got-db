c99b6c024c82131aa79ea53ee778e620627fb7ff
Ayub Mohamud <a.mohamud1@hotmail.com>
2019-12-05 12:45:02 +0000

    inserted data

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,73 +1,53 @@
 BEGIN;
 
-DROP TABLE IF EXISTS characters, houses, weapons, powers, honour CASCADE;
+DROP TABLE IF EXISTS characters, results, wallet, CASCADE;
 
 
 CREATE TABLE characters (
     id SERIAL PRIMARY KEY,
-    first_name VARCHAR(100) NOT NULL,
-    last_name VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    house VARCHAR(100) NOT NULL,
+    weapon VARCHAR(100) NOT NULL,
     honour INTEGER NOT NULL
 );
 
-CREATE TABLE houses (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    character_id INTEGER REFERENCES characters(id) 
-);
+CREATE TABLE results (
+id SERIAL PRIMARY KEY,
+dead_result VARCHAR(250) NOT NULL,
+survive_result VARCHAR(250) NOT NULL,
+wallet_id INTEGER
 
-CREATE TABLE weapons (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    character_id INTEGER REFERENCES characters(id) 
-);
 
-INSERT INTO characters(first_name,last_name,honour) VALUES
-('Jon', 'Snow',100),
-('Daenerys', 'Targaryen', 140),
-('Ice', 'King', 230),
-('Ramsay', 'Bolton', 80),
-('Petyr', 'Baelish', 40),
-('Arya', 'Stark', 120),
-('Cersei', 'Lannister', 40),
-('Robb', 'Stark', 90),
-('Jamie', 'Lannister', 110),
-('Tyrion', 'Lannister', 2),
-('Stannis', 'Baratheon', 90),
-('Balon', 'Greyjoy', 70),
-('Oberyn', 'Martell', 115),
-('Margaery', 'Tyrell', 3),
-('Brienn', 'of Tarth', 113),
-('Yara', 'Greyjoy', 86),
-('Ellaria', 'Sand', 94),
-('Red', 'Woman', 133),
-('Missandei', 'of Naath', 23),
-('Olena', 'Martell', 1);
+)
 
-INSERT INTO houses(name) VALUES
-('House Stark'),
-('House Targaryen'),
-('House Bolton'),
-('House Baelish'),
-('House Lannister'),
-('House Greyjoy'),
-('House Martell'),
-('House Tyrell'),
-('House Tarth'),
-('House Baratheon'),
-('No Allegiances');
 
-INSERT INTO weapons(name) VALUES
-('broad sword'),
-('dagger'),
-('wildfire'),
-('poison'),
-('whip'),
-('axe'),
-('mind'),
-('short sword'),
-('bow and arrow'),
-('dragon'),
-('direwolf');
+
+
+
+
+
+INSERT INTO characters(name,house,weapon,honour) VALUES
+('Jon Snow', 'House Stark','broad sword', 100)
+('Daenerys Targaryen','House Targaryen','dragons' ,140),
+('Ice King', 'No Allegiance','army of the dead', 230),
+('Ramsay Bolton','House Bolton', 'mind', 80),
+('Petyr Baelish', 'House Baelish', 'mind',40),
+('Arya Stark', 'House Stark', ' short sword' , 120),
+('Cersei Lannister', 'House Lannister', 'mind' , 30),
+('Robb Stark', 'House Stark', 'direwolf', 90),
+('Jamie Lannister','House Lannister', ' broad sword' , 101),
+('Tyrion Lannister', 'House Lannister', 'mind', 65),
+('Stannis Baratheon', 'House Baratheon', 'brute force', 23),
+('Balon Greyjoy','House Greyjoy', 'entertainment' 7),
+('Oberyn Martell', 'House Martell', 'dual swords' 115),
+('Margaery Tyrell','House Martell', 'persuasion' 3),
+('Brienn of Tarth','House Tarth', 'broad sword', 113),
+('Yara Greyjoy', 'House Greyjoy' , 'ships', 86),
+('Ellaria Sand','House Martell', 'whip', 94),
+('Red Woman','No Allegiance', 'magic', 133),
+('Missandei of Naath','No Allegiance','nothing',1 ),
+('Olena Martell' , 'House Martell','poison', 2);
+
+
 
 COMMIT; 
