184f8c7f09c6abb0b5278398aad2e4b9a5a47b3b
Reda <redahaq@gmail.com>
2019-12-06 13:56:22 +0000

    edit html copy

--- src/queries/getData.js
+++ src/queries/getData.js
@@ -1,6 +1,5 @@
 const dbConnection = require("../database/db_connection");
 
-
 const getData = (cb) => {
     dbConnection.query('SELECT name, house, weapon FROM characters', (err,res)=>{
         if(err) return cb(err);
