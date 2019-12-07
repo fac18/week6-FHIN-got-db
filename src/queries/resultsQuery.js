cb5d160d69d14a677310813461a19d5e97864087
Reda <redahaq@gmail.com>
2019-12-07 20:17:57 +0000

    style table - add padding, thead style, row borders, mobile first, hover colour, update h1 text with got font img in html, fix checkbox typo

--- src/queries/resultsQuery.js
+++ src/queries/resultsQuery.js
@@ -2,7 +2,8 @@ const dbConnection = require("../database/db_connection");
 
 
 const resultAlive = (cb) => {
-    dbConnection.query('SELECT BOTTOM 1 * FROM status', (err,res)=>{
+    // dbConnection.query('SELECT BOTTOM 1 * FROM status', (err,res)=>{
+    dbConnection.query('SELECT TOP 5 mortality FROM status', (err,res)=>{
         if(err) return cb(err);
         console.log('get data call = res.rows: ' + res.rows);
         cb(null, res.rows);
