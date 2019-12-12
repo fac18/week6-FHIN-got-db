c3d318809a5dd793368851e4c6bb26c6fa830404
Reda <redahaq@gmail.com>
2019-12-12 11:06:35 +0000

    add ajax request to landingscript

--- src/router.js
+++ src/router.js
@@ -8,8 +8,10 @@ const router = (request, response) => {
 
     } else if (endpoint.startsWith("/public")) {handlers.handlePublic(request, response, endpoint)
     } 
+    // change name from login to /game
     else if (endpoint.startsWith("/login")) {handlers.handleLogin(request, response, endpoint);
     }
+    //don't need logout endpoint as redirect to "/"
     else if (endpoint.startsWith("/logout")) {handlers.handleLogout(request, response, endpoint);
     }
     else if (endpoint.startsWith("/result")) { handlers.handleSubmit (request, response, endpoint);
