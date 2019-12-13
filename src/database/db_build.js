84da699ffebaf2fd0d731974601d9417164a9edb
Ayub Mohamud <a.mohamud1@hotmail.com>
2019-12-13 12:44:19 +0000

    db tests

--- src/database/db_build.js
+++ src/database/db_build.js
@@ -7,7 +7,6 @@ const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 
 // query takes a string as its first argument 
 
-dbConnection.query(sql,(error,res)=>{
-    if (error) throw error;
-    console.log('game of thrones table created with results', res);
-});
+const runDbBuild = cb => dbConnection.query(sql, cb)
+
+module.exports = runDbBuild
